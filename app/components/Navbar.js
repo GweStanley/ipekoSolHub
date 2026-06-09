cfd44ef4d5b386ba2a52707322287065682c9ff5
GweStanley <pathfinderlabs1@gmail.com>
2026-06-09 15:00:23 +0100

    Nav update

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,20 +1,37 @@
+'use client'
+
+import { useState } from 'react'
 import '../styles/navbar.css'
 
 export default function Navbar() {
+
+  const [open, setOpen] = useState(false)
+
   return (
+
     <nav className="navbar">
+
       <div className="logo">
         iPekosol Business Hub
       </div>
 
-      <ul className="nav-links">
+      <button
+        className="menu-btn"
+        onClick={() => setOpen(!open)}
+      >
+        Menu☰
+      </button>
+
+      <ul className={`nav-links ${open ? 'active' : ''}`}>
+
         <li><a href="/">Home</a></li>
         <li><a href="/portfolio">Portfolio</a></li>
-        <li><a href="/marketplace">Marketplace</a></li>
+        <li><a href="/marketplace">Shop</a></li>
         <li><a href="/testimonials">Testimonials</a></li>
         <li><a href="/dashboard">Admin</a></li>
 
       </ul>
+
     </nav>
   )
 }
